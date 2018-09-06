7e57c8d7c2aa9ee5b93d57675f9369ed3a6b9a07
Ulf Zackrisson <ulmezz@gmail.com>
2018-09-06 14:14:51 +0200

    Made some fixes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,6 +2,30 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://superman:thebest1@ds123012.mlab.com:23012/webshop', { useNewUrlParser: true })
+  .then(() => console.log('connection successful'))
+  .catch((err) => console.error(err));
+
+//var router = express.Router();
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  username: {type: String, required: true},
+  password: {type: String, required: true}
+}, {collection: 'user'});
+
+var User = mongoose.model('User', userSchema);
+
+router.get('/', function(req, res, next) {
+  User.find()
+      .then(function(data) {
+          res.json(data);
+      });
+});
+
 /* GET home page. */
 router.get('/home', function(req, res, next) {
   //res.render('index', { title: 'Express' });
